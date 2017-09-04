de1bf22ade906b7226c89e9a75cd00ed3731eeda
Adam YH LEE <adam@bigtuna.io>
2017-09-04 11:36:49 -0700

    Remove outdated files

--- keymap_plain.c
+++
@@ -1,8 +0,0 @@
-#include "keymap_common.h"
-const action_t PROGMEM fn_actions[] = {};
-const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* 0: qwerty */
-    KEYMAP(Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   BSPC, \
-           A,   S,   D,   F,   G,   H,   J,   K,   L,   RSFT,ENT, \
-           Z,   X,   C,   V,   SPC,      B,   N,   M ,  RALT, RCTL ),
-};
